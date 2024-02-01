43c7b6700a85dd7d32bb413e73a24fd4b5478086
fedor44 <joobappe@gmail.com>
2024-02-02 08:24:54 +0900

    streamlit 추가

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-load_dotenv()
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
@@ -8,44 +7,74 @@ from langchain_openai.chat_models import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain import hub
+import streamlit as st
+import tempfile
+import os
 
-# load pdf file
-loader = PyPDFLoader("sejong.pdf")
-pages = loader.load_and_split()
+# load env
+load_dotenv()
 
-# split data
-text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size = 300,
-    chunk_overlap  = 20,
-    length_function = len,
-    is_separator_regex = False,
-)
-texts = text_splitter.split_documents(pages)
+# method for uploading file
+def pdf_to_document(uploaded_file):
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = PyPDFLoader(temp_filepath)
+    pages = loader.load_and_split()
+    return pages
 
-# embedding with openai
-embeddings = OpenAIEmbeddings()
+# title
+st.title("ChatPDF")
+st.write("---")
 
-# save data to Chroma
-vectorstore = Chroma.from_documents(texts, embeddings)
+# upload file
+uploaded_file = st.file_uploader("Choose a file")
+st.write("---")
 
-# make a question
-question = "세종텔레콤 회장님 학력 좀 알려줘"
-llm = ChatOpenAI(model_name = "gpt-3.5-turbo", temperature = 0)
-#llm = ChatOpenAI(model_name = "gpt-4-1106-preview", temperature = 0)
+if uploaded_file is not None:
+    pages = pdf_to_document(uploaded_file)
 
-# Retrieve and generate using the relevant snippets of the blog.
-retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
-rag_chain = (
-        {"context": retriever, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
-)
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size = 300,
+        chunk_overlap  = 20,
+        length_function = len,
+        is_separator_regex = False,
+    )
+    texts = text_splitter.split_documents(pages)
 
-result = rag_chain.invoke(question)
-print(result)
+    # embedding with openai
+    embeddings = OpenAIEmbeddings()
+
+    # save data to Chroma
+    vectorstore = Chroma.from_documents(texts, embeddings)
+
+    # make a question
+    st.header("PDF에게 질문하세요.")
+    question = st.text_input("질문을 입력 하세요")
+
+    if st.button("질문하기"):
+        with st.spinner("wait for it..."):
+            # make a question
+            #question = "세종텔레콤 회장님 학력 좀 알려줘"
+            #llm = ChatOpenAI(model_name = "gpt-3.5-turbo", temperature = 0)
+            llm = ChatOpenAI(model_name = "gpt-4-0125-preview", temperature = 0)
+
+            # Retrieve and generate using the relevant snippets of the blog.
+            retriever = vectorstore.as_retriever()
+            prompt = hub.pull("rlm/rag-prompt")
+            rag_chain = (
+                    {"context": retriever, "question": RunnablePassthrough()}
+                    | prompt
+                    | llm
+                    | StrOutputParser()
+            )
+
+            result = rag_chain.invoke(question)
+            st.write(result)
+            #print(result)
 
 #result = llm.invoke(question)
 #print(result)
